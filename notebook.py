b51dd88364a5571ca030a4f4b2f70e2c7bb74816
Alexershov86 <alexershov86@mail.ru>
2023-07-21 19:05:09 +0300

    Добавил методы add и show в класс Notebook

--- notebook.py
+++ notebook.py
@@ -17,4 +17,19 @@ class Notebook:
     def set_listOfNotes(self, list_of_notes):
         self.__listOfNotes = list_of_notes
     
-   
+    def add_note(self):
+        _id = self.__counter + 1
+        title = input('title: ')
+        message = input('message: ')
+        date = datetime.now().strftime('%B %d, %Y, %H:%M:%S')
+        self.__listOfNotes.append(Note(_id, date, title, message))
+        self.__counter += 1
+
+    def show_note(self, indx):
+        self.__listOfNotes[indx].show_note()
+
+    def show_notebook(self):
+        for note in self.__listOfNotes:
+            note.show_note()
+
+
