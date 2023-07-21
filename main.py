0a12eb622732b41d697227a4551759d1b146e960
Alexershov86 <alexershov86@mail.ru>
2023-07-21 20:46:24 +0300

    Добавил выборку по дате

--- main.py
+++ main.py
@@ -1,25 +1,5 @@
 from notebook import Notebook
-from read_write_json import ReadWriteJson
 from menu import Menu
 
-# my_notebook = Notebook()
-# my_notebook.add_note()
-# my_notebook.add_note()
-# my_notebook.add_note()
-# my_notebook.show_notebook()
-
-# ReadWriteJson().write(my_notebook)
-
-# some_notebook = ReadWriteJson().read()
-# print()
-# some_notebook.show_notebook()
-# print()
-
-# some_notebook.change_note(some_notebook.find_note(3))
-
-# ReadWriteJson().write(some_notebook)
-
-# new_notebook = ReadWriteJson().read()
-# new_notebook.show_notebook()
 
 Menu().start(Notebook())
